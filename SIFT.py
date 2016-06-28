7a5628ceb43dbabf7bf8dbb8fc3a97c3640c7084
AvaKum <avaneesh.paper@gmail.com>
2016-06-28 20:10:16 +0530

    Add files via upload

---
+++ SIFT.py
@@ -0,0 +1,25 @@
+#Imports
+import cv2
+import numpy as np
+
+# Read an image. TEst it with a relevant image eg. smiling girl
+img = cv2.imread("thumb_DSC_0363_1024.jpg")
+
+#COnvert image to gray scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Create a SIFT object
+sift = cv2.xfeatures2d.SIFT_create(0, 3, 0.03, 10, 1.6);
+
+#Detect keypoints
+kp = sift.detect(gray, None)
+
+#Draw keypoints
+img=cv2.drawKeypoints(gray,kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imwrite('sift_keypoints.jpg',img)
+
+# Get descriptors
+kp, des = sift.compute(gray, kp)
+
+print (len(kp))
+print (des[0])
